340e4be9dd6560196d05ae92919aa12300a2aa52
Ian Murray <ianmurrays@gmail.com>
2011-10-08 17:12:44 -0300

    Adds login logic and chat room controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 FayeTutorial::Application.routes.draw do
   get  '/login' => 'sessions#new', :as => :login
+  post '/login' => 'sessions#create', :as => :login
+  get  '/chatroom' => 'chats#room', :as => :chat
   
   # The priority is based upon order of creation:
   # first created -> highest priority.
