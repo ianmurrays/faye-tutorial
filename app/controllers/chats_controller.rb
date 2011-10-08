6bebcbf3aa97d490f5d48bc90c72f22fc2c5f8cc
Ian Murray <ianmurrays@gmail.com>
2011-10-08 17:40:45 -0300

    Adds basic chat funcionality

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,4 +1,13 @@
+require "net/http"
+
 class ChatsController < ApplicationController
   def room
+  end
+  
+  def new_message
+    faye = URI.parse 'http://localhost:9292/faye'
+    message = {:username => session[:username], :msg => params[:message]}.to_json
+    Net::HTTP.post_form(faye, :message => {:channel => '/messages/public', :data => message}.to_json)
+    render :nothing => true
   end
 end
