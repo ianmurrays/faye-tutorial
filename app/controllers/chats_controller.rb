180953c07a75ca3d306d085d8cf65fdf41b975df
Ian Murray <ianmurrays@gmail.com>
2011-10-09 15:14:37 -0300

    Adds private conversations

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -7,8 +7,17 @@ class ChatsController < ApplicationController
   
   def new_message
     faye = URI.parse 'http://localhost:9292/faye'
-    message = {:username => session[:username], :msg => params[:message]}.to_json
-    Net::HTTP.post_form(faye, :message => {:channel => '/messages/public', :data => message}.to_json)
+    
+    # Check if the message is private
+    if recipient = params[:message].match(/@(.+) (.+)/)
+      
+      # It is private, send it to the recipient's channel
+      message = {:username => session[:username], :msg => recipient.captures.second}.to_json
+      Net::HTTP.post_form(faye, :message => {:channel => "/messages/private/#{recipient.captures.first}", :data => message}.to_json)
+    else
+      message = {:username => session[:username], :msg => params[:message]}.to_json
+      Net::HTTP.post_form(faye, :message => {:channel => '/messages/public', :data => message}.to_json)
+    end
     
     respond_to do |f|
       f.js
